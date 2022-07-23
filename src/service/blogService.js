31704d5435b073a40f17f99c4e246dcac36fa51a
mstoelting89 <michaelstoelting@gmail.com>
2022-07-23 15:24:02 +0200

    make blog visible for normal user
    (cherry picked from commit 0d6ba1c6093c6c71790c58788342c35fca3225b1)

--- src/service/blogService.js
+++ src/service/blogService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export default {
     getBlogPosts() {
-        const token = localStorage.getItem("token");
+        //const token = localStorage.getItem("token");
 
         const header = {
             'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization': `Bearer ${token}`,
+            /*'Authorization': `Bearer ${token}`,*/
             'Access-Control-Allow-Origin' : '*',
             'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
         }
