64347c41fc417d174e7ab05af80e4e8b06e83f43
michaelstoelting <ms@reflect-media.de>
2025-04-17 10:52:05 +0200

    change api url

--- src/service/globals.js
+++ src/service/globals.js
@@ -1,5 +1,5 @@
 //const HOST_NAME = 'http://localhost:8886';
-const HOST_NAME = 'https://api.ghp.stoelting-michael.de';
+const HOST_NAME = 'https://ghp.stoelting-michael.de';
 
 export default {
     HOST_NAME
